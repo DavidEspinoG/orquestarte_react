8709f8e93e55fb25e85626ca06fd5e760ecef789
David Espino <davidespinog@gmail.com>
2022-12-03 18:26:56 -0600

    contact form

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Template } from "../components/Template";
-
 function Contact(){
+    const [name, setName ] = useState('');
+    const [mail, setMail ] = useState('');
+    const [message, setMessage ] = useState('');
     return(
         <>
             <Template>
-                <p>Contacto</p>
+                <div className="contenedor">
+                    <div className="form-container">
+                    <h2> ¿Interesad@? Dejanos tus datos y nos pondremos en contacto </h2>
+                        <form className="form" 
+                            onSubmit={(e) => {
+                                e.preventDefault()
+                                console.log(name, mail, message)
+                            }}
+                        >
+                            <label htmlFor="name">Tu nombre</label>
+                            <input type='text' id="name" value={name} 
+                                onChange={(e)=> {
+                                    setName(e.target.value)
+                                }}
+                            ></input>
+                            <label htmlFor="email">Tu E-Mail</label>
+                            <input type='email' id="email" value={mail}
+                                onChange={(e)=> {
+                                    setMail(e.target.value)
+                                }}
+                            ></input>
+                            <label htmlFor="message">Mensaje</label>
+                            <textarea id="message" value={message}
+                                onChange={(e)=> {
+                                    setMessage(e.target.value)
+                                }}
+                            />
+                            <button type="submit">Enviar</button>
+                        </form>
+                    </div>
+                </div>
             </Template>
         </>
     )
