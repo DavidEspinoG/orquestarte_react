5694b18b84c4dc519be9d672979a571616293d13
David Espino <davidespinog@gmail.com>
2023-08-21 12:55:39 -0600

    Add useState to Login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { Template } from "../components/Template";
 import { Title } from "../components/Title";
+import { useState } from "react";
 
 const Login = () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
   return (
     <>
       <Template>
         <Title>Iniciar sesión</Title>
-        {/* <div className="main-content-container"> */}
           <div className="form-container">
-            <form className="form">
+            <form className="form" onSubmit={(e) => {
+              e.preventDefault();
+              console.log({email, password})
+            }}>
               <label htmlFor="e-mail">E-mail</label>
-              <input required type='text' id="e-mail"></input>
+              <input required type='email' id="e-mail"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}  
+              >
+              </input>
               <label htmlFor="password">Contraseña</label>
-              <input required type='password' id="password"></input>
+              <input required type='password' id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
               <button type="submit">Enviar</button>
             </form>
           </div>
-        {/* </div> */}
-        
       </Template>
     </>
   )
