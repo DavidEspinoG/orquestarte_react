1e0950adeb616969abdcfd11a55854fcd5143823
David Espino <davidespinog@gmail.com>
2023-08-22 11:36:52 -0600

    Add protection to admin

--- src/routes/Admin.js
+++ src/routes/Admin.js
@@ -6,11 +6,22 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
+  const userName = useSelector(state => state.user.name);
+  const isAdmin = useSelector(state => state.user.isAdmin);
   const navigate = useNavigate();
+  useEffect(() => {
+    if(!isAdmin){
+      navigate('/');
+    }
+  })
+
   return(
   <>
-    <Template pagos={true}>
+    <Template home={true}>
       <Title>Admin</Title>
+      <div className="contenedor">
+        <h3>Hola, {userName}</h3>
+      </div>
     </Template>
   </>)
 }
