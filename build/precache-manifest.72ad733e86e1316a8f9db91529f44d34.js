88eaf1db5ad7b15883a29e01f5b83c6cbd99fbc3
David Espino <davidespinog@gmail.com>
2023-08-29 14:10:20 -0600

    Update build

---
+++ build/precache-manifest.72ad733e86e1316a8f9db91529f44d34.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f34284c030a6c9629322",
+    "url": "/static/css/main.8067f0c7.chunk.css"
+  },
+  {
+    "revision": "f34284c030a6c9629322",
+    "url": "/static/js/main.05b39e40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9dda847aab72068c1022",
+    "url": "/static/css/2.a3f33864.chunk.css"
+  },
+  {
+    "revision": "9dda847aab72068c1022",
+    "url": "/static/js/2.e996fdca.chunk.js"
+  },
+  {
+    "revision": "fc90243bb02315287288134fc8978398",
+    "url": "/static/media/aboutus.fc90243b.png"
+  },
+  {
+    "revision": "afbd121f93cfe97dc230974094d9565f",
+    "url": "/static/media/about2.afbd121f.png"
+  },
+  {
+    "revision": "bb92e37ef66fadfe50a808707abeaf2f",
+    "url": "/static/media/about3.bb92e37e.png"
+  },
+  {
+    "revision": "a79dfe4500fa5cf02214a48f85c9ab36",
+    "url": "/static/media/galeria1.a79dfe45.jpg"
+  },
+  {
+    "revision": "ced36f65e1fc00c8da70001c7c2a6b32",
+    "url": "/static/media/galeria2.ced36f65.jpg"
+  },
+  {
+    "revision": "62a4b9c8489b5d59808263fa2d999a33",
+    "url": "/static/media/galeria3.62a4b9c8.jpg"
+  },
+  {
+    "revision": "41f20ab9a8b50a8fb047e2322797069a",
+    "url": "/static/media/galeria4.41f20ab9.jpeg"
+  },
+  {
+    "revision": "31f8f0a4e1a8e226f97933944622918b",
+    "url": "/static/media/galeria5.31f8f0a4.jpeg"
+  },
+  {
+    "revision": "a6d36377a9b14071f545d608f526509d",
+    "url": "/static/media/galeria6.a6d36377.jpg"
+  },
+  {
+    "revision": "c14bdb2e49a96e66e7292a58fea1e870",
+    "url": "/static/media/galeria7.c14bdb2e.jpg"
+  },
+  {
+    "revision": "f0fe77f4d9e2636e9ffc8c954dbfd2a3",
+    "url": "/static/media/galeria4.f0fe77f4.jpeg"
+  },
+  {
+    "revision": "9f5d062f2df5e7d8682bfdf68a4ccc26",
+    "url": "/static/media/galeria5.9f5d062f.jpeg"
+  },
+  {
+    "revision": "10e03c820bb2d1539aa93e9a10fa4f26",
+    "url": "/static/media/galeria6.10e03c82.jpg"
+  },
+  {
+    "revision": "b1ab09af470273394a20097b4ac6fd2e",
+    "url": "/static/media/galeria7.b1ab09af.jpg"
+  },
+  {
+    "revision": "d6b7301d0b59833f3364807db23e88e3",
+    "url": "/static/media/testimonial1.d6b7301d.jpeg"
+  },
+  {
+    "revision": "7f098a3ee7776372d149ca0f461e356a",
+    "url": "/static/media/testimonial2.7f098a3e.jpeg"
+  },
+  {
+    "revision": "14337141f6f68dd168074f5c83eda9b2",
+    "url": "/static/media/testimonial3.14337141.jpeg"
+  },
+  {
+    "revision": "efb1be4cc38157fbd7d5a4102377bc47",
+    "url": "/index.html"
+  }
+];
